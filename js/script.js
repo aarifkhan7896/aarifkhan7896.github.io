2304ef58c7fc64f4a6cedd6acfe86c20fc94dbd1
Aarif Khan <aarifkhan7896@gmail.com>
2021-11-25 18:10:02 +0530

    modified

--- js/script.js
+++ js/script.js
@@ -1,53 +1,28 @@
+// Toggle Navbar
+$(document).ready(function(){
+  $(".fa-bars").click(function(){
+    $("#navbar").slideToggle();
+  })
 
-      AOS.init({
-        duration: 1000,
-      });
+// scroll effect on header
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if(scroll>300){
+      $("nav").css({"background-color":"#0a192f","padding":"5px 20px","transition":"0.5s"});
+    }else{
+      $("nav").css({"background-color":"#1a1a1a","padding":"15px 20px","transition":"0.5s"});
+    }
+  })
 
-      $(document).ready(function () {
-        $(window).scroll(function () {
-          var scroll = $(window).scrollTop();
-          if (scroll > 300) {
-            $(".nav").css("background", "#43809f ");
-            $("#topArrow").css("display", "block");
-          } else {
-            $(".nav").css("background", "#39a7a7");
-            $("#topArrow").css("display", "none");
-          }
-        });
-      });
+// toggle projects
+  $("#btnMore").click(function(){
+    $("#showMore").slideToggle();
+    $("#btnMore").html('<i class="fas fa-angle-up fa-2x"></i>');
+  })
+})
 
-      document.onreadystatechange = function () {
-        if (document.readyState !== "complete") {
-          document.querySelector("body").style.visibility = "hidden";
-          document.querySelector(".loader").style.visibility = "visible";
-        } else {
-          document.querySelector(".loader").style.display = "none";
-          document.querySelector("body").style.visibility = "visible";
-        }
-      };
-      var sidenav = document.getElementById("navbar-links");
-      function openNav() {
-        sidenav.style.width = "200px";
-      }
-      function closeNav() {
-        sidenav.style.width = "0";
-      }
-      function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-      }
-
-      var icon = document.getElementById("dark");
-      function darkMode() {
-        document.body.classList.toggle("dark-theme");
-        if(document.body.classList.contains("dark-theme")){
-          icon.src = "img/light.png";
-        }else{
-          icon.src = "img/dark.png";
-        }
-      }
-
-      function imgFunction(smallImg){
-        let img = document.getElementById("bigImg");
-        img.src = smallImg.src;
-      }
+// scroll top
+function topHead() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}

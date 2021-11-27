5b90673048017779c134b037887aabf103cca2a4
Aarif Khan <aarifkhan7896@gmail.com>
2021-11-27 13:57:38 +0530

    modified

--- js/script.js
+++ js/script.js
@@ -19,6 +19,28 @@ $(document).ready(function(){
     $("#showMore").slideToggle();
     $("#btnMore").html('<i class="fas fa-angle-up fa-2x"></i>');
   })
+
+// owl-carousel
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    dots:false,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:1
+      },
+      1200:{
+        items:1
+      }
+    }
+  })
+
 })
 
 // scroll top
